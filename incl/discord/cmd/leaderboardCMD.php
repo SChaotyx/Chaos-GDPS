106a072b87bb8b7b5bf0156d6d4ac391a2790226
SChaotyx <Agustinangelcab@gmail.com>
2019-12-19 07:20:35 -0300

    LeaderboardCMD: now is top 20

--- incl/discord/cmd/leaderboardCMD.php
+++ incl/discord/cmd/leaderboardCMD.php
@@ -18,9 +18,9 @@ if(empty($_POST["type"])){
 $type = $_POST["type"];
 $tag = $_POST["tagID"];
 
-$query = $db->prepare("SELECT * FROM users WHERE $type AND isBanned = 0 ORDER BY $type DESC LIMIT 10");
+$query = $db->prepare("SELECT * FROM users WHERE $type AND isBanned = 0 ORDER BY $type DESC LIMIT 20");
 if($type === "creatorPoints"){
-    $query = $db->prepare("SELECT * FROM users WHERE $type AND isCreatorBanned = 0 ORDER BY $type DESC LIMIT 10");
+    $query = $db->prepare("SELECT * FROM users WHERE $type AND isCreatorBanned = 0 ORDER BY $type DESC LIMIT 20");
 }
 $query->execute();
 if($query->rowCount() == 0){
@@ -50,23 +50,33 @@ switch($type){
 foreach($array as $row) {
     $pos ++;
     switch($pos){
-        case 1: $icontop = $icon_top1; break;
-        case 2: $icontop = $icon_top10; break;
-        case 3: $icontop = $icon_top50; break;
-        case 4: $icontop = $icon_top100; break;
-        case 5: $icontop = $icon_top200; break;
-        case 6: $icontop = $icon_top500; break;
-        case 7: $icontop = $icon_top1000; break;
-        case 8: $icontop = $icon_globalrank; break;
-        case 9: $icontop = $icon_globalrank; break;
-        case 10: $icontop = $icon_globalrank; break;
+        case 1: $icontop = $icon_top1; $posn = "1 #";break;
+        case 2: $icontop = $icon_top10; $posn = "2 #";break;
+        case 3: $icontop = $icon_top10; $posn = "3 #";break;
+        case 4: $icontop = $icon_top100; $posn = "4 #";break;
+        case 5: $icontop = $icon_top100; $posn = "5 #";break;
+        case 6: $icontop = $icon_top100; $posn = "6 #";break;
+        case 7: $icontop = $icon_top200; $posn = "7 #";break;
+        case 8: $icontop = $icon_top200; $posn = "8 #";break;
+        case 9: $icontop = $icon_top200; $posn = "9 #";break;
+        case 10: $icontop = $icon_top200; $posn = "10#";break;
+        case 11: $icontop = $icon_top500; $posn = "11#";break;
+        case 12: $icontop = $icon_top500; $posn = "12#";break;
+        case 13: $icontop = $icon_top500; $posn = "13#";break;
+        case 14: $icontop = $icon_top500; $posn = "14#";break;
+        case 15: $icontop = $icon_top500; $posn = "15#";break;
+        case 16: $icontop = $icon_top1000; $posn = "16#";break;
+        case 17: $icontop = $icon_top1000; $posn = "17#";break;
+        case 18: $icontop = $icon_top1000; $posn = "18#";break;
+        case 19: $icontop = $icon_top1000; $posn = "19#";break;
+        case 20: $icontop = $icon_top1000; $posn = "20#";break;
     }
-    $lol .= "$icontop `$pos#` - $Licon `".$dis->charCount($row[$type])."` __**".$row['userName']."**__\n───────────────────\n";
+    $lol .= "$icontop `$posn` | $Licon `".$dis->charCount($row[$type])."` | __**".$row['userName']."**__\n";
 }
 
 
 $data = array(
-    "content"=> "<@".$_POST["tagID"].">, Here TOP 10 Leaderboard based on $Ltype",
+    "content"=> "<@".$_POST["tagID"].">, Here TOP 20 Leaderboard based on $Ltype",
     'embed'=> [
         "title"=> "$Licon __Top 10 Leaderboards!!!__",
         "description"=> "───────────────────\n".$lol,
